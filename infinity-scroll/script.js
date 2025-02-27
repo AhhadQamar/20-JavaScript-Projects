ee2775e70ee198908c6ce73bae0f29c7b64a3769
Ahhad Qamar <ahhadqamar1@gmail.com>
2025-02-27 15:34:50 +0500

    added a function to add to add insted of doing it manually

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,19 +1,32 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 let photosArray = [];
+// Create a helper function
+function setAttributes(element, attribute) {
+    // using for-in loop
+    for (const key in attribute) {
+        element.setAttribute(key,attribute[key]);
+    } 
+}
 // Create Elements for links and photos,add to DOM
 function displayPhotos() {
     // run function for each object in photosArray
     photosArray.forEach((photo) => {
     // create <a> to link to unsplash
      const item = document.createElement('a');
-     item.setAttribute('href',photo.links.html);
-     item.setAttribute('target','_blank');
+    //  using helper function to add to DOM
+    setAttributes(item,{
+        href: photo.links.html,
+        target: '_blank',
+    });
     //  Create <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    // using helper function to add to DOM
+    setAttributes(img,{
+        src: photo.urls.regular,
+        alt: photo.alt_description,
+        title: photo.alt_description,
+    });
     // put <img> in <a> and both of them in imageContainer
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -29,8 +42,7 @@ async function getPhotos() {
         photosArray = await response.json();
         displayPhotos();
     } catch (error) {
-        
+        getPhotos();
     }
 }
 getPhotos();
-
