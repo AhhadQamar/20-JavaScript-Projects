a413349b9c89447570654fb6bde116930d76a122
Ahhad Qamar <ahhadqamar1@gmail.com>
2025-03-10 20:52:32 +0500

    formated code

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,74 +1,77 @@
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader')
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
-// Check if all mages were loaded and increase the number of images loaded 
+// Check if all mages were loaded and increase the number of images loaded
 function imageLoaded() {
-    imagesLoaded++;
-    if (imagesLoaded === totalImages) {
-        ready = true;
-        loader.hidden = true;
-        count = 10;
-        apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-    }
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    count = 10;
+    apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  }
 }
 // Create a helper function
 function setAttributes(element, attribute) {
-    // using for-in loop
-    for (const key in attribute) {
-        element.setAttribute(key,attribute[key]);
-    } 
+  // using for-in loop
+  for (const key in attribute) {
+    element.setAttribute(key, attribute[key]);
+  }
 }
 // Create Elements for links and photos,add to DOM
 function displayPhotos() {
-    imagesLoaded = 0;
-    totalImages = photosArray.length;
-    // run function for each object in photosArray
-    photosArray.forEach((photo) => {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // run function for each object in photosArray
+  photosArray.forEach((photo) => {
     // create <a> to link to unsplash
-     const item = document.createElement('a');
+    const item = document.createElement("a");
     //  using helper function to add to DOM
-    setAttributes(item,{
-        href: photo.links.html,
-        target: '_blank',
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
     });
     //  Create <img> for photo
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     // using helper function to add to DOM
-    setAttributes(img,{
-        src: photo.urls.regular,
-        alt: photo.alt_description,
-        title: photo.alt_description,
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
     });
     // Event listener, check when each is finished loading
-    img.addEventListener('load',imageLoaded);
+    img.addEventListener("load", imageLoaded);
     // put <img> in <a> and both of them in imageContainer
     item.appendChild(img);
     imageContainer.appendChild(item);
-     });
+  });
 }
 // Get photos from unsplash api
 async function getPhotos() {
-    const apiKey = 'kLjYnvoMT4cwfZJf4otu6jz3lAYcoxydFUFXmzYoINo';
-    let count = 5;
-    let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-    try {
-        const response = await fetch(apiUrl);
-        photosArray = await response.json();
-        displayPhotos();
-    } catch (error) {
-        displayPhotos();
-    }
+  const apiKey = "kLjYnvoMT4cwfZJf4otu6jz3lAYcoxydFUFXmzYoINo";
+  let count = 5;
+  let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    displayPhotos();
+  }
 }
 // Check to see if scrolling near bottom of page, load more photos
-window.addEventListener('scroll',() => {
-    // Checking if the sum of total height of the window and the length the user has scrolled down is >= to the total height of every thing that is loaded and if is not visible and is ready true  
- if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+window.addEventListener("scroll", () => {
+  // Checking if the sum of total height of the window and the length the user has scrolled down is >= to the total height of every thing that is loaded and if is not visible and is ready true
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
     ready = false;
     getPhotos();
- }
+  }
 });
 // Run on Load
 getPhotos();
--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,60 +1,58 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterButton = document.getElementById('twitter');
-const newQuoteButton = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterButton = document.getElementById("twitter");
+const newQuoteButton = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 let apiQuotes = [];
 // Show Loading
 function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 // Hide Loading
 function complete() {
-    loader.hidden = true;
-    quoteContainer.hidden = false;
+  loader.hidden = true;
+  quoteContainer.hidden = false;
 }
 // Show New Quote
 function newQuote() {
-    loading();
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // Checking for NULL author
-    if (quote.author == 'Anonymous') {
-        authorText.textContent = 'Unknown'
-    } else{
-        authorText.textContent = quote.author;
-    }
-    // decreasing the size of a quote according to it's length
-    if (quote.text.length > 120) {
-        quoteText.classList.add('long-quote');
-    }else{
-        quoteText.classList.remove('long-quote');
-    }
-    // set quote, hide loader
-    quoteText.textContent = quote.text;
-    complete();
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Checking for NULL author
+  if (quote.author == "Anonymous") {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // decreasing the size of a quote according to it's length
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // set quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
 }
-
 
 // Getting Quotes
 async function getQuotes() {
-    loading()
-const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
-try {
+  loading();
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
-} catch (error) {  
-}
+  } catch (error) {}
 }
 // Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl,'_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
 }
 // Event LIstenners
-newQuoteButton.addEventListener('click',newQuote);
-twitterButton.addEventListener('click',tweetQuote);
+newQuoteButton.addEventListener("click", newQuote);
+twitterButton.addEventListener("click", tweetQuote);
 // Run On Load
 getQuotes();
